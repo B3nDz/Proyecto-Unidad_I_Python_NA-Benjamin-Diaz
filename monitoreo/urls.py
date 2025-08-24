dede7034d51c7d20bcc1bf73481014d8e85a3211
Benja <benjamindiazcastillo@gmail.com>
2025-08-24 17:20:26 -0400

    Primer commit - Proyecto_Unidad_I

---
+++ monitoreo/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from dispositivos.views import inicio, panel_dispositivos
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', inicio),
+    path('panel/', panel_dispositivos),
+]
